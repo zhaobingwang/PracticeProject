440492ec03455e8559ddabed4088cee32dedcb74
Route8 <zhaobingwang@foxmail.com>
2018-08-01 09:51:17 +0800

    iis test

---
+++ Project.WebAPI/Controllers/IISThreadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using NLog;
+
+namespace Project.WebAPI.Controllers
+{
+    public class IISThreadController : ApiController
+    {
+        private static Logger logger;
+
+        public IISThreadController()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(string param)
+        {
+            //DoSomething();
+            Task.Run(() =>
+            {
+                DoSomething(param);
+            });
+            return Ok($"Get message:{param}");
+        }
+
+        public void DoSomething(string param)
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    logger.Info($" #{nameof(DoSomething)} [{param}]");
+                    Thread.Sleep(3000);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Warn($" #{nameof(DoSomething)} [{param}] \n \t {ex}");
+            }
+        }
+    }
+}
--- src/Client/Project.APIClient/Program.cs
+++ src/Client/Project.APIClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project.APIClient
@@ -10,8 +12,40 @@ namespace Project.APIClient
     {
         static void Main(string[] args)
         {
-            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            Console.WriteLine(Convert.ToInt64(ts.TotalMilliseconds).ToString()); 
+            var baseUri = "http://localhost/Project.WebAPI/api/";
+
+            Console.WriteLine("请求状态 \t 信息 \t 调用线程ID \t 时间");
+
+            var tasks = new Task[5];
+            for (int i = 0; i < 5; i++)
+            {
+
+                tasks[i] = Task.Run(() =>
+                {
+                    var client = new HttpClient();
+                    client.BaseAddress = new Uri(baseUri);
+                    var response = client.GetAsync($"iisthread/get?param={Guid.NewGuid()}").Result;
+                    Console.WriteLine($" {response.StatusCode} \t {response.Content.ReadAsStringAsync().Result} \t {Thread.CurrentThread.ManagedThreadId} \t {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss fff")}");
+                });
+            }
+
+            #region MyRegion
+            //Task t1 = Task.Run(() =>
+            //{
+            //    var client = new HttpClient();
+            //    client.BaseAddress = new Uri(baseUri);
+            //    var response = client.GetAsync("iisthread/get").Result;
+            //    Console.WriteLine($" {response.StatusCode} \t {response.Content.ReadAsStringAsync().Result} \t {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss fff")}");
+            //}).ContinueWith((t) => {
+            //    Console.WriteLine($"aa:{t.Exception.InnerException.Message}");
+            //}, TaskContinuationOptions.OnlyOnFaulted); 
+            #endregion
+
+
+            Console.WriteLine("A");
+            Task.WaitAll(tasks);
+            Console.WriteLine("B");
+            //Thread.Sleep(1000);
         }
     }
 }
