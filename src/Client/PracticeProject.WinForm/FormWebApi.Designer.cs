a7298d7b5e3f7e5c067811563ee4e02575d313c0
Route8 <zhaobingwang@foxmail.com>
2018-08-07 08:21:34 +0800

    winform

---
+++ src/Client/PracticeProject.WinForm/FormWebApi.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PracticeProject.WinForm
+{
+    partial class FormWebApi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTry = new System.Windows.Forms.Button();
+            this.listViewUsers = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderCreateDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderModifyDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnTry
+            // 
+            this.btnTry.Location = new System.Drawing.Point(37, 13);
+            this.btnTry.Name = "btnTry";
+            this.btnTry.Size = new System.Drawing.Size(75, 23);
+            this.btnTry.TabIndex = 0;
+            this.btnTry.Text = "try";
+            this.btnTry.UseVisualStyleBackColor = true;
+            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            // 
+            // listViewUsers
+            // 
+            this.listViewUsers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderCreateDate,
+            this.columnHeaderModifyDate});
+            this.listViewUsers.Location = new System.Drawing.Point(50, 135);
+            this.listViewUsers.Name = "listViewUsers";
+            this.listViewUsers.Size = new System.Drawing.Size(519, 156);
+            this.listViewUsers.TabIndex = 1;
+            this.listViewUsers.UseCompatibleStateImageBehavior = false;
+            this.listViewUsers.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "姓名";
+            // 
+            // columnHeaderCreateDate
+            // 
+            this.columnHeaderCreateDate.Text = "创建时间";
+            this.columnHeaderCreateDate.Width = 200;
+            // 
+            // columnHeaderModifyDate
+            // 
+            this.columnHeaderModifyDate.Text = "修改时间";
+            this.columnHeaderModifyDate.Width = 200;
+            // 
+            // FormWebApi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listViewUsers);
+            this.Controls.Add(this.btnTry);
+            this.Name = "FormWebApi";
+            this.Text = "FormWebApi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.ListView listViewUsers;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderCreateDate;
+        private System.Windows.Forms.ColumnHeader columnHeaderModifyDate;
+    }
+}
---
+++ src/Client/PracticeProject.WinForm/FormWebApi.cs
@@ -0,0 +1,62 @@
+﻿using Project.Framework.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace PracticeProject.WinForm
+{
+    public partial class FormWebApi : Form
+    {
+        public FormWebApi()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTry_Click(object sender, EventArgs e)
+        {
+            var users = GetUsers();
+            listViewUsers.Items.Clear();
+            foreach (var user in users)
+            {
+                ListViewItem lv = new ListViewItem();
+                lv.Text = user.Name;
+                lv.SubItems.Add(user.CreateDate.ToString());
+                lv.SubItems.Add(user.ModifyDate.ToString());
+                listViewUsers.Items.Add(lv);
+            }
+        }
+
+        private List<User> GetUsers()
+        {
+            string json = GetJsonDataFrooAPI();
+            var result = JsonConvert.DeserializeObject<List<User>>(json);
+            return result;
+        }
+
+        private string GetJsonDataFrooAPI()
+        {
+            string url = "http://localhost/Project.WebAPI/api/user/getusers?pageIndex=1&pageSize=10";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.ContentType = "application/json";
+
+            string result = string.Empty;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                result = reader.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
