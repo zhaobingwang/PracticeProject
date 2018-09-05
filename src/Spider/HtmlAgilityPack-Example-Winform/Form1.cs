153504cf04dbb13263c3d599c77befca4233799c
Route8 <zhaobingwang@foxmail.com>
2018-09-05 09:24:35 +0800

    temp-commit

---
+++ src/Spider/HtmlAgilityPack-Example-Winform/Form1.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HtmlAgilityPack_Example_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = "https://www.toutiao.com/a6596994489816973838/";
+
+            //var web1 = new HtmlWeb();
+            //var doc1 = web1.LoadFromBrowser(url, o =>
+            //{
+            //    var webBrowser = (WebBrowser)o;
+
+            //    // WAIT until the dynamic text is set
+            //    return !string.IsNullOrEmpty(webBrowser.Document.("uiDynamicText").InnerText);
+            //});
+            //var t1 = doc1.DocumentNode.SelectSingleNode("//div[@class=article-content]").InnerText;
+
+            var web2 = new HtmlWeb();
+            var doc2 = web2.LoadFromBrowser(url, html =>
+            {
+                // WAIT until the dynamic text is set
+                return !html.Contains("<div class=\"article-content\"></div>");
+            });
+            var t2 = doc2.DocumentNode.SelectSingleNode("//div[@class=article-content]").InnerText;
+
+            //MessageBox.Show("Text 1: " + t1);
+            MessageBox.Show("Text 2: " + t2);
+        }
+    }
+}
---
+++ src/Spider/HtmlAgilityPack-Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace HtmlAgilityPack_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FromWeb();
+            ParseToutiaoFromWeb();
+        }
+
+        #region Parser
+        public static void FromWeb()
+        {
+            var html = "https://www.toutiao.com/";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+            Console.WriteLine($"Node Name:{node.Name}\n{node.OuterHtml}");
+        }
+        public static void ParseToutiaoFromWeb()
+        {
+            var html = "https://www.toutiao.com/a6596994489816973838/";
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(html);
+            //var node = htmlDoc.DocumentNode.SelectSingleNode("//div[@class=article-content]");
+            //var node = htmlDoc.DocumentNode.SelectSingleNode("//script/BASE_DATA");
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//script[contains(text(), 'articleInfo')]");
+
+            Console.WriteLine($"Node Name:{node.Name}\n{node.InnerHtml}");
+
+
+        }
+        
+        #endregion
+    }
+}
