7c21e7000ca4e318bdd111347d463dd680a7d70c
Route8 <zhaobingwang@foxmail.com>
2018-10-17 16:14:32 +0800

    update some details

---
+++ test/ConsoleApp.Test/RedisOp.cs
@@ -0,0 +1,32 @@
+﻿using PracticeProject.Core.Cache;
+using System;
+using System.Configuration;
+
+namespace ConsoleApp.Test
+{
+    class RedisOp
+    {
+        private static int DbIndex = 15;
+        //private static string RedisHost = "127.0.0.1:6379";
+        private static string CustomKey = string.Empty;
+
+        static void Main(string[] args)
+        {
+            //string RedisConnectionString = ConfigurationManager.ConnectionStrings["RedisExchangeHosts"]?.ConnectionString;
+            string SysCustomKey = ConfigurationManager.AppSettings["redisKey"] ?? "";
+
+            var db = new RedisOperator(DbIndex);
+            db.CustomKey = SysCustomKey;
+
+            Console.WriteLine("start");
+            for (int i = 0; i < 100; i++)
+            {
+                string key = $"A{i}";
+                var result = db.StringSet(key, i, TimeSpan.FromSeconds(1 * (i + 1)));
+
+                Console.WriteLine(key);
+            }
+            Console.WriteLine("end");
+        }
+    }
+}
--- test/PracticeProject.Core.Tests/RedisOperatorTest.cs
+++ test/PracticeProject.Core.Tests/RedisOperatorTest.cs
@@ -4,28 +4,50 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using System.Configuration;
+using StackExchange.Redis;
 
 namespace PracticeProject.Core.Tests
 {
+    [Trait("Redis","String")]
     public class RedisOperatorTest
     {
         private int DbIndex = 15;
         private string RedisHost = "127.0.0.1:6379";
-        private string CustomKey = string.Empty;
+        private string CustomKey = "Demo:Test:";
 
+        RedisOperator db;
         public RedisOperatorTest()
         {
+
+            db = new RedisOperator(DbIndex, RedisHost);
+            db.CustomKey = CustomKey;
         }
 
-        [Fact]
-        public void StringSetTestWithExpectedParameters()
+        [Fact(DisplayName = "保存单个Key,Value正常测试")]
+        public void StringSet_WithExpectedParameters()
         {
             // arrange
-            var db = new RedisOperator(DbIndex, RedisHost);
-            db.CustomKey = CustomKey;
+
 
             // act
-            var result = db.StringSet("a", "test value");
+            var result = db.StringSet("single", "single value", TimeSpan.FromMinutes(1));
+
+            // assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "保存多个Key,Value正常测试")]
+        public void StringSetMultiKV_WithExpectedParameters()
+        {
+            // arrange
+            List<KeyValuePair<RedisKey, RedisValue>> list = new List<KeyValuePair<RedisKey, RedisValue>>();
+
+            list.Add(new KeyValuePair<RedisKey, RedisValue>("A", "1"));
+            list.Add(new KeyValuePair<RedisKey, RedisValue>("B", "2"));
+            list.Add(new KeyValuePair<RedisKey, RedisValue>("C", "3"));
+
+            // act
+            var result = db.StringSet(list);
 
             // assert
             Assert.True(result);
