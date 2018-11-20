32d88753b04f9bc014ea4ada81cd6bf17b313b1a
Route8 <zhaobingwang@foxmail.com>
2018-11-20 15:17:58 +0800

    initialize the project of code nodes

---
+++ src/CodeNodes/CodeNodes.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeNodes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
